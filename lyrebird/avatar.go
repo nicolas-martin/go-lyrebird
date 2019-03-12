a95f66f9a9f19f2e605bd2e67c119517143d6bf0
nicolas-martin <nicolas.martin.0986@gmail.com>
2019-03-12 16:06:17 -0400

    Added Profile and Generated avatar endpoint

--- lyrebird/avatar.go
+++ lyrebird/avatar.go
@@ -2,19 +2,29 @@ package lyrebird
 
 import (
 	"context"
+	"time"
 )
 
 const (
-	generateURL = "https://avatar.lyrebird.ai/api/v0/generate"
+	generateURL  = "https://avatar.lyrebird.ai/api/v0/generate"
+	generatedURL = "https://avatar.lyrebird.ai/api/v0/generated"
+	profileURL   = "https://avatar.lyrebird.ai/api/v0/profile"
 )
 
 // AvatarService handles communication with the Avatar related
 // methods of the Lyrebird API
 type AvatarService service
 
+// AvatarError represents am Lyrebird avatar error
+type AvatarError struct {
+	Description *string `json:"description,omitempty"`
+}
+
 // Avatar represents a Lyrebird avatar
 type Avatar struct {
-	Description *string `json:"description,omitempty"`
+	CreatedAt *time.Time `json:"created_at,omitempty"`
+	Text      *string    `json:"text,omitempty"`
+	URL       *string    `json:"url,omitempty"`
 }
 
 func (a Avatar) String() string {
@@ -23,6 +33,12 @@ func (a Avatar) String() string {
 
 type generateBody struct {
 	Text string
+}
+
+type Profile struct {
+	DisplayName string `json:"display_name"`
+	Email       string `json:"email"`
+	UserID      string `json:"user_id"`
 }
 
 // Generate a sound clip from a string
@@ -42,4 +58,36 @@ func (as *AvatarService) Generate(ctx context.Context, text string) (*Avatar, *R
 	}
 
 	return avatar, resp, nil
+}
+
+// Generated returns a list of generated voice clips
+func (as *AvatarService) Generated(ctx context.Context) (*[]Avatar, *Response, error) {
+	req, err := as.client.NewRequest("GET", generatedURL, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	avatar := new([]Avatar)
+	resp, err := as.client.Do(ctx, req, avatar)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return avatar, resp, nil
+}
+
+// Profile returns the user's profile
+func (as *AvatarService) Profile(ctx context.Context) (*Profile, *Response, error) {
+	req, err := as.client.NewRequest("GET", profileURL, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	profile := new(Profile)
+	resp, err := as.client.Do(ctx, req, profile)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return profile, resp, nil
 }
